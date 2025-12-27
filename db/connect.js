1fd3b6567a33724f05f768e2abecf30f1a92d11e
Mohamed Zoraa <mohamedzoraa100@gmail.com>
2025-12-27 17:32:38 +0100

    some errors fix

--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,11 @@
 const mongoose=require("mongoose")
 
-const connectDb=(dbURI)=>{
-    return mongoose.connect(dbURI)
+const connectDb=async(dbURI)=>{
+     try {
+        await mongoose.connect(dbURI)
+     } catch (error) {
+        console.log("mongodb connection error :"+error)
+        process.exit(0)
+     }
 }
 module.exports=connectDb
